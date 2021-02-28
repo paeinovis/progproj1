7375c8adff242a8068af1492be06ab9a8993ad98
paeinovis <c.swanson@ufl.edu>
2021-02-28 18:31:03 -0500

    18/27 on p2

--- LinkedList.h
+++ LinkedList.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <vector>
 #ifndef PROGPROJ1_LINKEDLIST_H
 #define PROGPROJ1_LINKEDLIST_H
 using namespace std;
@@ -13,12 +14,12 @@ class LinkedList {
         struct Node {
             T data;
             Node* next;
+            Node* prev;
         };
 
     unsigned int num_nodes;
     Node* head_ptr;
     Node* tail_ptr;
-    Node* next;
 
     // Behaviors
     void PrintForward() const {
@@ -28,7 +29,6 @@ class LinkedList {
             current_node = current_node->next;
         }
     }
-
     void PrintReverse() const {
         T* node_array = new T[(int) NodeCount()];
         Node* current_node = head_ptr;
@@ -39,6 +39,7 @@ class LinkedList {
         for (int i = 0; i < (int) num_nodes; i++) {
             cout << node_array[i] << endl;
         }
+        delete[] node_array;
     }
     //void PrintForwardRecursive(const Node* node) const;
     //void PrintReverseRecursive(const Node* node) const;
@@ -47,19 +48,75 @@ class LinkedList {
     unsigned int NodeCount() const {
         return num_nodes;
     }
-    //void FindAll(vector<Node*>& outData, const T& value) const;
-    //const Node* Find(const T& data) const;
-    //Node* Find(const T& data);
-    //const Node* GetNode(unsigned int index) const;
-    //Node* GetNode(unsigned int index);
-    //Node* Head(){
-    //    return head_ptr;
-    //}
-    //Const Node* Head() const; // FIXME
-    //Node* Tail(){
-    //    return tail_ptr;
-    //}
-    //const Node* Tail() const; // FIXME
+    void FindAll(vector<Node*>& outData, const T& value) const{
+        Node* current_node = head_ptr;
+        Node* prev_node = nullptr;
+        while (current_node != nullptr) {
+            if (current_node->data == value) {
+                outData.push_back(current_node);
+                current_node->prev = prev_node;
+            }
+            if (current_node == head_ptr) {
+                prev_node = head_ptr;
+            }
+            else {
+                prev_node = prev_node->next;
+            }
+            current_node = current_node->next;
+        }
+    }
+    const Node* Find(const T& data) const {
+        Node* current_node = head_ptr;
+        while (current_node != nullptr) {
+            if (current_node->data == data) {
+                return current_node;
+            }
+            current_node = current_node->next;
+        }
+        return nullptr;
+    }
+    Node* Find(const T& data) {
+        Node* current_node = head_ptr;
+        while (current_node != nullptr) {
+            if (current_node->data == data) {
+                return current_node;
+            }
+            current_node = current_node->next;
+        }
+        return nullptr;
+    }
+    const Node* GetNode(unsigned int index) const {
+        Node *current_node = head_ptr;
+        for (unsigned int i = 0; i < index; i++) {
+            current_node = current_node->next;
+            if (current_node == nullptr) {
+                throw out_of_range ("e");
+            }
+        }
+        return current_node;
+    }
+    Node* GetNode(unsigned int index) {
+        Node* current_node = head_ptr;
+        for (unsigned int i = 0; i < index; i++) {
+            current_node = current_node->next;
+            if (current_node == nullptr) {
+                throw out_of_range ("e");
+            }
+        }
+        return current_node;
+    }
+    Node* Head(){
+        return head_ptr;
+    }
+    const Node* Head() const {
+        return head_ptr;
+    }
+    Node* Tail(){
+        return tail_ptr;
+    }
+    const Node* Tail() const{
+        return tail_ptr;
+    }
 
     // Insertion
     void AddHead(const T &data) {
@@ -110,8 +167,22 @@ class LinkedList {
     //void Clear();
     //
     //// Operators
-    //const T& operator[](unsigned int index) const;
-    //T& operator[](unsigned int index);
+    const T& operator[](unsigned int index) const {
+        try {
+            const Node *op_ptr = GetNode(index);
+            return op_ptr->data;
+        } catch (exception) {
+            throw out_of_range ("e");
+        }
+    }
+    T& operator[](unsigned int index){
+        try {
+            Node *op_ptr = GetNode(index);
+            return op_ptr->data;
+        } catch (exception) {
+            throw out_of_range ("e");
+        }
+    }
     //bool operator==(const LinkedList<T>& rhs) const;
     //LinkedList<T>& operator=(const LinkedList<T>& rhs);
 
@@ -122,17 +193,16 @@ class LinkedList {
             head_ptr = nullptr;
             tail_ptr = nullptr;
         }
+    // Copy Constructor
         LinkedList(const LinkedList <T> &list){
-            num_nodes = list.size();
-            AddHead(list[0]);
-            AddTail(list[(int) num_nodes - 1]);
-            Node* current_node = head_ptr;
-            for (int i = 1; i < (int) num_nodes - 1; i++) {
-                current_node->next = list[i];
-                current_node = current_node->next;
+            int num_iter = num_nodes - 1;
+            this->num_nodes = list.num_nodes;
+            this->head_ptr = list.head_ptr;
+            this->tail_ptr = list.tail_ptr;
+            for (int i = 1; i < num_iter; i++) {
+                AddTail(list.GetNode(i)->data);
             }
         }
-
 
     // Destructor
     ~LinkedList(){
@@ -145,7 +215,27 @@ class LinkedList {
         delete head_ptr;
     }
 };
-
+//for (int i = 1; i < (int) num_nodes - 1; i++) {
+//                current_node = list.GetNode(i);
+//                current_node = current_node->next;
+//                num_nodes++;
+//            }
+//for (int i = 1; i < (int) num_nodes - 1; i++) {
+//                AddTail(list[i]);
+//                current_node = current_node->next;
+//            }
+//while(current_node != nullptr){
+//                AddTail(current_node->data);
+//                current_node = current_node->next;
+//            }
+//num_nodes = list.size();
+//            AddHead(list[0]);
+//            AddTail(list[(int) num_nodes - 1]);
+//            Node* current_node = head_ptr;
+//            for (int i = 1; i < (int) num_nodes - 1; i++) {
+//                current_node->next = list[i];
+//                current_node = current_node->next;
+//            }
 
 
 #endif //PROGPROJ1_LINKEDLIST_H
